9f37328aa87481677fc88bf3abb9c9f9642640f3
Victor Laurentino do Nascimento <53540002+PitzTech@users.noreply.github.com>
2024-05-17 14:51:22 -0300

    update migrations

--- db/migration/1715964503184-Create_Courses_Table.ts
+++ db/migration/1715964503184-Create_Courses_Table.ts
@@ -38,19 +38,48 @@ export class CreateCoursesTable1715964503184 implements MigrationInterface {
       })
     )
 
-    await queryRunner.createForeignKey(
-      tableName, // courses
-      new TableForeignKey({
-        columnNames: ["user_id", "course_id"],
-        referencedColumnNames: ["id"],
-        referencedTableName: joinedTable, // users,
-        onDelete: "CASCADE"
+    await queryRunner.createTable(
+      new Table({
+        name: joinTableName,
+        columns: [
+          {
+            name: "id",
+            type: "serial",
+            isPrimary: true
+          },
+          {
+            name: "course_id",
+            type: "int",
+            isNullable: false
+          },
+          ,
+          {
+            name: "user_id",
+            type: "int",
+            isNullable: false
+          }
+        ],
+        foreignKeys: [
+          {
+            columnNames: ['course_id'],
+            referencedTableName: tableName,
+            referencedColumnNames: ['id'],
+            onDelete: "CASCADE"
+          },
+          {
+            columnNames: ['user_id'],
+            referencedTableName: joinedTable,
+            referencedColumnNames: ['id'],
+            onDelete: "CASCADE"
+          }
+        ]
       })
     )
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.dropTable(tableName)
+    await queryRunner.dropTable(joinTableName)
   }
 
 }
