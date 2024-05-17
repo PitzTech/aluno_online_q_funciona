59383ccae1f8b628e061ad0e727104ed352a3573
Victor Laurentino do Nascimento <53540002+PitzTech@users.noreply.github.com>
2024-05-16 23:53:33 -0300

    update migration

--- db/migration/1715884199060-Create_User_Collumn_Password.ts
+++ db/migration/1715884199060-Create_User_Collumn_Password.ts
@@ -1,19 +1,19 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner, TableColumn } from "typeorm";
 import { entityName } from '../entity/User';
 
 const tableName = entityName
 
 export class CreateUserCollumnPassword1715884199060 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(
-      `ALTER TABLE ${tableName} ADD COLUMN password varchar(100);`
-    )
+    await queryRunner.addColumn(tableName, new TableColumn({
+      name: 'password',
+      type: "varchar(100)",
+      isNullable: true
+    }))
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(
-      `ALTER TABLE ${tableName} DROP COLUMN password;`
-    )
+    await queryRunner.dropColumn(tableName, 'password')
   }
 
 }
